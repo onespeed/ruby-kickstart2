e254772dde9ab7f4d43633523e1037f0ea39b27b
Tom Erb <onespeed@gmail.com>
2012-04-03 20:47:42 +0000

    session1 challenge 7: Make test and FAILING string-search function.

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -5,4 +5,27 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  retstr = ""
+  string.scan( /r./i ).each do |mtch|
+    retstr << mtch[1]
+  end
+  retstr
 end
+
+
+
+def test
+  td = {
+   "are you really learning Ruby?" => "eenu",
+   "Katy Perry is on the radio!"  =>  "rya",
+   "Pirates say arrrrrrrrr"       =>  "arrrrrrrr" 
+  }
+
+  td.each do | key, val |
+    failmsg = ( pirates_say_arrrrrrrrr(key) == val ? 'pass' : "FAIL" )
+    puts %Q{PSA(#{key}) = #{pirates_say_arrrrrrrrr(key)} ?==? #{val} -- #{failmsg} }
+  end 
+
+end
+
+test
