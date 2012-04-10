338fe89bba9479a31d946a7330d0a8b642a94c31
Tom Erb <onespeed@gmail.com>
2012-04-10 03:45:18 +0000

    session1 challenge 3 - another loop that seems unnecessary

--- session2/challenge/3_array.rb
+++ session2/challenge/3_array.rb
@@ -5,4 +5,19 @@
 # "abcdefg".every_other_char  # => "aceg"
 # "".every_other_char         # => ""
 
+class String
+  def every_other_char
+    str = ""
+      (0...self.length).select {|i| i.even?} .each do |i|
+        str << self[i]
+      end
+    str
+  end
+
+end
+
+# I'd like a non-loop solution for this, something like
+# self.each_with_index do 2
+# It seems like it should be a one-liner
+# (tom, 4/7/2012)
 
