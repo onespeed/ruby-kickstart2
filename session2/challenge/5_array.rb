41527f067fc93f31bf350e93ee058a2b4550aa02
Tom Erb <onespeed@gmail.com>
2012-04-10 19:56:58 +0000

    session 2, challenges 5 & 6, loopy solution for 6

--- session2/challenge/5_array.rb
+++ session2/challenge/5_array.rb
@@ -13,3 +13,8 @@
 # mod_three [7]  # => [1]
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
+
+def mod_three(ary)
+  ary.map {|x| x % 3 } .select {|x| x != 0 }
+end
+
--- session2/challenge/6_array.rb
+++ session2/challenge/6_array.rb
@@ -14,3 +14,22 @@
 # prime_chars? ['ab', 'cd']       # => false
 # prime_chars? ['a', 'bcd']       # => false
 # prime_chars? ['a', 'b', 'cd']   # => false
+
+def prime_chars?(ary)
+  # I think this could be a one-liner with something like this
+  #ary.map {|st| st.length } # this makes an array of the lengths, now join?
+
+  # but for now, lets' go with the .each I know...
+  totln = 0
+  ary.each { |st| totln += st.length }
+
+  # apparently the test spec wants 0 and 1 to return false
+  return false if totln == 0 || totln == 1
+  # now check for prime
+  (2...totln).each do |i|
+    return false if totln % i == 0
+  end
+  # if you got through the loop, then the totln is prime
+  return true
+
+end
