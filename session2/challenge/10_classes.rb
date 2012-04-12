870b627797b03701a65bcdbb9fd2f9c4f0a0bf81
Tom Erb <onespeed@gmail.com>
2012-04-12 18:01:42 +0000

    s2 ch10 simpler version with attr_accessor

--- session2/challenge/10_classes.rb
+++ session2/challenge/10_classes.rb
@@ -21,12 +21,11 @@ class Person
     @age = age
   end
 
-#attr_accessor name
-  def name ; @name ; end
-  def name= (name) ; @name = name ; end
-
-  def age ; @age ; end
-  def age= (age) ; @age = age ; end
+  attr_accessor 'name', 'age'
+#def name ; @name ; end
+#def name= (name) ; @name = name ; end
+#def age ; @age ; end
+#def age= (age) ; @age = age ; end
 
   def birthday
     @age += 1
